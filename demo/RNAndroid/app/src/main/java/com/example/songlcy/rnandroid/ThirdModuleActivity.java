3c0476b0e5b36fa8245aed556243f113fb271923
songxiaoliang <563609104@qq.com>
2019-03-18 10:28:01 +0800

    添加第三方依赖案例

---
+++ demo/RNAndroid/app/src/main/java/com/example/songlcy/rnandroid/ThirdModuleActivity.java
@@ -0,0 +1,14 @@
+package com.example.songlcy.rnandroid;
+
+import com.facebook.react.ReactActivity;
+
+import javax.annotation.Nullable;
+
+public class ThirdModuleActivity extends ReactActivity {
+
+    @Nullable
+    @Override
+    protected String getMainComponentName() {
+        return "ThirdModule";
+    }
+}
